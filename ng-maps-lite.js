dfb7f9f76c3c86affd7e468fc105fe1fc014eedf
Jofferson Tiquez <jofftiquez@gmail.com>
2017-03-01 21:29:36 +0800

    Test element link

--- ng-maps-lite.js
+++ ng-maps-lite.js
@@ -8,65 +8,64 @@
 *
 */
 
-angular.module('fooModule', [])
-        .provider('foo', function () {
-            this.setBar = function () {
-                this.bar = true;
-            };
-            
-            this.$get = function () {
-                return this;
-            };
-        });
+console.log('ng-maps-lite here 0');
 
-// angular.module('ng-maps-lite', []).provider('$ngMapsLite', function () {
-//     var config = {
-//         height:300,
-//     };
+(function(angular){
 
-//     this.config = function(options){
-//         config.height = options.height || config.height;
-//         return config;
-//     }
+    var ngMapsLiteModule = angular.module('ngMapsLite', []);
 
-//     this.setBar = function () {
-//         this.bar = true;
-//     };
+    ngMapsLiteModule.provider('$ngMapsLite', function () {
 
-//     this.$get = function () {
-//         return this;
-//     };
+        console.log('ng-maps-lite here 1')
 
-// }).directive('ng-maps-lite', ['$ngMapsLite', function($ngMapsLite){
-//     var map;
-//     var mapOpt;
-//     var marker;
-//     var markerOpt;
-//     var searchBox;
-//     var searchBoxInput;
-//     var geocoder = new google.maps.Geocoder;
-//     var infoWindow = new google.maps.InfoWindow;
-//     var directionsService = new google.maps.DirectionsService;
-//     var directionsRenderer = new google.maps.DirectionsRenderer;
-//     var currentCenter = {};
+        var config = {};
 
-//     return {
-//         restrict: 'EA',
-//         scope: {
-//             map: '=map',
-//             marker: '=marker',
-//             height: '=height',
-//             placesCallback: '=placesCallback',
-//             multiplePlaces: '=multiplePlaces',
-//             readOnly: '=readOnly'
-//         },
-//         templateUrl: 'app/directives/mc-maps/mc-maps.html',
-//         css: 'app/directives/mc-maps/mc-maps.css',
-//         link: link,
-//     };
+        this.configMap = function(options){
+            config.height = options.height || config.height;
+            return config;
+        }
 
-//     function link(scope, element, attrs){
-//         scope.height = $ngMapsLite.setHeight()
-//     }
+        this.getHeight = function () {
+            return config.height;
+        };
 
-// }]);
+        this.$get = function () {
+            return this;
+        };
+
+    })
+    
+    ngMapsLiteModule.directive('ng-maps-lite', ['$ngMapsLite', function($ngMapsLite){
+
+        console.log('ng-maps-lite here 2')
+
+        var map;
+        var mapOpt;
+        var marker;
+        var markerOpt;
+        var searchBox;
+        var searchBoxInput;
+        var geocoder = new google.maps.Geocoder;
+        var infoWindow = new google.maps.InfoWindow;
+        var directionsService = new google.maps.DirectionsService;
+        var directionsRenderer = new google.maps.DirectionsRenderer;
+        var currentCenter = {};
+
+        return {
+            restrict: 'E',
+            scope: {
+                height: '=height',
+            },
+            templateUrl: 'ng-maps-lite.html',
+            link: link
+        };
+
+        function link(scope, element, attrs){
+            console.log('ng-maps-lite here 4')
+            console.log(scope.height)
+            scope.height = $ngMapsLite.getHeight()
+        }
+
+    }]);
+
+})(angular);
