6e7a3db93e1ec9fb32990b731fd99da36fdafe96
Jofferson Tiquez <jofftiquez@gmail.com>
2017-03-01 23:42:23 +0800

    Added - config for map - config for marker - config for searchBox  - Can now accept dynamic css styles - display map using setCenter function - display maker on map center

--- ng-maps-lite.js
+++ ng-maps-lite.js
@@ -8,26 +8,80 @@
 *
 */
 
-console.log('ng-maps-lite here 0');
-
 (function(angular){
 
     var ngMapsLiteModule = angular.module('ngMapsLite', []);
 
     ngMapsLiteModule.provider('$ngMapsLite', function () {
 
-        console.log('ng-maps-lite here 1')
+        this.readOnly = false;
+        this.mapHeight = 350;
+        this.searchBox = {placeholder: 'Enter location'};
+        this.map = { zoom: 15 };
+        this.marker = {};
+        this.place;
 
-        var config = {};
+        this.setmapOptions = function (options) {
+            this.map = options;
+        };
 
-        this.configMap = function(options){
-            config.height = options.height || config.height;
-            return config;
+        this.getMapOptions = function(){
+            return this.map;
         }
 
-        this.getHeight = function () {
-            return config.height;
-        };
+        this.setMapHeight = function(height){
+            this.mapHeight = height;
+        }
+
+        this.getMapHeight = function(){
+            return this.mapHeight;
+        }
+
+        this.markerConfig = function(options){
+            this.marker.icon = options.icon;
+        }
+
+        this.getMarkerConfig = function(){
+            return this.marker;
+        }
+
+        this.searchBoxConfig = function(options){
+            this.searchBox.style = options.css;
+            this.searchBox.placeholder = options.placeholder || 'Enter location';  
+        }
+
+        this.getsearchBoxConfig = function(){
+            return this.searchBox;
+        }
+
+        this.getHeight = function(){
+            return this.height;
+        }
+
+        this.setReadOnly = function(readOnly){
+            this.readOnly = readOnly;
+        }
+
+        this.getReadOnly = function(){
+            return this.readOnly;
+        }
+
+        this.setCenter = function(center){
+            // object {lat:val, lng:val}
+            this.center = center;
+        }
+
+        this.getCenter = function(){
+            return this.center;
+        }
+
+        this.setPlace = function(place){
+            this.place = place;
+        }
+        
+        this.returnPlace = function(){
+            return this.place;
+        }
 
         this.$get = function () {
             return this;
@@ -35,15 +89,14 @@ console.log('ng-maps-lite here 0');
 
     })
     
-    ngMapsLiteModule.directive('ng-maps-lite', ['$ngMapsLite', function($ngMapsLite){
-
-        console.log('ng-maps-lite here 2')
+    ngMapsLiteModule.directive('ngMapsLite', function () {
 
         var map;
         var mapOpt;
         var marker;
         var markerOpt;
         var searchBox;
+        var center;
         var searchBoxInput;
         var geocoder = new google.maps.Geocoder;
         var infoWindow = new google.maps.InfoWindow;
@@ -52,20 +105,44 @@ console.log('ng-maps-lite here 0');
         var currentCenter = {};
 
         return {
-            restrict: 'E',
-            scope: {
-                height: '=height',
-            },
-            templateUrl: 'ng-maps-lite.html',
-            link: link
+            template: `
+                <input placeholder="{{ searchBoxPlaceHolder }}" type="text" id="searchBox" ng-hide="readOnly">
+                <div id="map" style="height:{{ mapHeight }}px; width:100%;"></div>
+            `,
+            controller: function ($scope, $q, $window, $timeout, $ngMapsLite) {
+                var ngMap = $ngMapsLite;
+                var searchBoxConfig = ngMap.getsearchBoxConfig();
+                var mapCenter = ngMap.getCenter();
+                var markerConfig = ngMap.getMarkerConfig();
+                var mapOptions = ngMap.getMapOptions();
+                $scope.mapHeight = ngMap.getMapHeight();
+                $scope.readOnly = ngMap.getReadOnly();
+                $scope.searchBoxPlaceHolder = searchBoxConfig.placeholder;
+                
+                // center = new google.maps.LangLng(mapCenter.lat, mapCenter.lng);                
+                
+                searchBoxInput = document.getElementById('searchBox');
+                for(var key in searchBoxConfig.style){
+                    if(searchBoxConfig.style.hasOwnProperty(key)){
+                        searchBoxInput.style[key] = searchBoxConfig.style[key];
+                    }
+                }
+                searchBox = new google.maps.places.SearchBox(searchBoxInput);
+                searchBox.addListener('places_changed', function(){});
+
+                mapOptions.center = mapCenter;
+                map = new google.maps.Map(document.getElementById('map'), mapOptions);
+                map.addListener('center_changed', function(){});
+
+                markerConfig.map = map;
+                markerConfig.position = mapCenter;
+                marker = new google.maps.Marker(markerConfig);
+                marker.addListener('click', function(){});
+                marker.addListener('dragstart', function(){});
+                marker.addListener('dragend', function(){});
+            }
         };
 
-        function link(scope, element, attrs){
-            console.log('ng-maps-lite here 4')
-            console.log(scope.height)
-            scope.height = $ngMapsLite.getHeight()
-        }
-
-    }]);
+    });
 
 })(angular);
